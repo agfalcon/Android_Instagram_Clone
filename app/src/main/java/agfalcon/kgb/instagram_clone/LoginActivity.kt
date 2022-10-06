c1d3c23b4417aa3de7112c764b10677bc754a6fa
agfalcon <agfalcon12@gmail.com>
2022-10-06 15:54:55 +0900

    Login & SignUp with Email function Complete~~⚡️

--- app/src/main/java/agfalcon/kgb/instagram_clone/LoginActivity.kt
+++ app/src/main/java/agfalcon/kgb/instagram_clone/LoginActivity.kt
@@ -1,11 +1,54 @@
 package agfalcon.kgb.instagram_clone
 
+import agfalcon.kgb.instagram_clone.databinding.ActivityLoginBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? = null
+    private lateinit var  binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        binding.emailLoginButton.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup(){
+        auth?.createUserWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())?.addOnCompleteListener {
+            task ->
+                if(task.isSuccessful){// Creating a user accout
+                    moveMainPage(task.result.user)
+                }else if(task.exception?.message.isNullOrEmpty()){ // Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }else{//Login if you have account
+                    signinEmail()
+                }
+        }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(binding.emailEdittext.text.toString(), binding.passwordEdittext.text.toString())?.addOnCompleteListener {
+            task->
+                if(task.isSuccessful){//Login
+                    moveMainPage(task.result.user)
+                }else{//Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if(user !=null){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
